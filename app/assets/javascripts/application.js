bff634fa0f14aa4cb8c58caee2b3080fd9d98ff3
dan@nerdi.com <dan@nerdi.com>
2013-12-02 16:29:19 +1100

    add early stage css and db seed"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
+/***
+ * Excerpted from "Agile Web Development with Rails",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+***/
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -7,8 +15,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
